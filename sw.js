d1b425370fe66228c4a2c92bbcb834b5facf1160
juan carlos Dioses guerrero <40503619+antoleod@users.noreply.github.com>
2025-09-05 09:12:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+
+// Simple PWA service worker for offline support
+const CACHE = 'scanner-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // Only cache same-origin GET requests
+  if (e.request.method === 'GET' && url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      }).catch(() => caches.match('./')))
+    );
+  }
+});
