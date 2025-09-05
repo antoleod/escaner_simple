29bb551e06578a06ee9de8bf0b21ca5772e83efe
juan carlos Dioses guerrero <40503619+antoleod@users.noreply.github.com>
2025-09-05 09:24:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-// Simple PWA service worker for offline support
-const CACHE = 'scanner-pro-v1';
+// SW v2 - bump cache to force update
+const CACHE = 'scanner-pro-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,27 +8,21 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(self.skipWaiting()));
 });
-
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))) 
   );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  // Only cache same-origin GET requests
   if (e.request.method === 'GET' && url.origin === location.origin) {
     e.respondWith(
       caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
-        const copy = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return r;
+        const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r;
       }).catch(() => caches.match('./')))
     );
   }
